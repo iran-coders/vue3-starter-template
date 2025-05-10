495a0eb2c0ff367ad50d30633068517d94d88366
xGooddevilx <behroziyan.mhrn@gmail.com>
2025-05-10 10:39:15 +0330

    feat(Services):Implement comments services & its composable

---
+++ src/composables/comments.composable.js
@@ -0,0 +1,32 @@
+import { computed, ref } from "vue";
+import { useLoading } from "./loading.composable";
+import { keyBy } from "@/utils";
+import CommentsService from "@/services/comments.service";
+
+export const useFetchComments = () => {
+    const { isLoading, startLoading, endLoading } = useLoading();
+    const comments = ref([]);
+    const commentsKeyById = computed(() => keyBy(comments.value, "id"));
+
+    /**
+     * @param {AxiosRequestConfig} [config]
+     */
+    const fetchComments = (config) => {
+        startLoading();
+        return CommentsService.getAll(config)
+            .then((response) => {
+                comments.value = response.data;
+                return response;
+            })
+            .finally(() => {
+                endLoading();
+            });
+    };
+
+    return {
+        comments,
+        commentsIsLoading: isLoading,
+        commentsKeyById,
+        fetchComments,
+    };
+};
