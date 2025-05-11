ac384732923b70624612da730f900d033ae9adf7
xGooddevilx <behroziyan.mhrn@gmail.com>
2025-05-11 09:11:21 +0330

    feat(Composables): Implement caching post logic

--- src/composables/posts.composable.js
+++ src/composables/posts.composable.js
@@ -1,6 +1,7 @@
 import { ref } from "vue";
 import { useLoading } from "./loading.composable";
 import PostsServices from "@/services/posts.services";
+import StorageService from "@/services/storage.service";
 
 export default function useFetchPost() {
     const { endLoading, isLoading, startLoading } = useLoading();
@@ -8,18 +9,23 @@ export default function useFetchPost() {
     const post = ref(null);
 
     function fetchPost(id) {
-        startLoading()
-        return PostsServices.getOneById(id).then((response) => {
-            post.value = response.data
-            return response
-        }).finally(()=>{
-            endLoading()
-        });
+        const cachedPosts = StorageService.get("cached-posts") || {};
+        if (cachedPosts[`post-${id}`]) return cachedPosts[`post-${id}`];
+        startLoading();
+        return PostsServices.getOneById(id)
+            .then((response) => {
+                post.value = response.data;
+                StorageService.set('cached-posts', { ...cachedPosts, [`post-${id}`]: response.data })
+                return response;
+            })
+            .finally(() => {
+                endLoading();
+            });
     }
 
     return {
         postIsLoading: isLoading,
         post,
-        fetchPost
+        fetchPost,
     };
 }
