2cf204ce63a234f7a675a4eada2463e01d676196
xGooddevilx <behroziyan.mhrn@gmail.com>
2025-05-11 08:18:00 +0330

    feat(View):Implement client-side filtering for comments

--- src/components/comments/CommentsFilterForm.vue
+++ src/components/comments/CommentsFilterForm.vue
@@ -3,25 +3,26 @@ import { useModelRef } from '@/composables/model.composable';
 import VForm from '../form/VForm.vue';
 import VInput from '../form/VInput.vue';
 import { t } from '@/services/language.service';
+import VSelect from '../form/VSelect.vue';
 export default {
     name: 'CommentsFilterForm',
     setup() {
 
 
-     const filters = useModelRef('modelValue');        
+        const filters = useModelRef('modelValue');
 
         const statusItems = [
             {
-                key:'PENDING',
-                text:t('Pending')
+                key: 'PENDING',
+                text: t('Pending')
             },
             {
-                key:'REJECTED',
-                text:t('Rejected')
+                key: 'REJECTED',
+                text: t('Rejected')
             },
             {
-                key:'CONFIRMED',
-                text:t('Confirmed')
+                key: 'CONFIRMED',
+                text: t('Confirmed')
             }
         ]
 
@@ -30,9 +31,16 @@ export default {
             statusItems
         }
     },
+    props: {
+        modelValue: {
+            type: Object,
+            required: true
+        }
+    },
     components: {
         VForm,
-        VInput
+        VInput,
+        VSelect
     },
     emits: ['update:modelValue'],
 
@@ -40,9 +48,16 @@ export default {
 </script>
 
 <template>
-    <VForm class="flex">
+    <VForm class="row">
         <div class="col-3">
             <VInput v-model="filters.searchQuery" :placeholder="$t('SearchInputPlaceholder')" id="searchQuery" />
+        </div>
+        <div class="col-3">
+            <VInput v-model="filters.email" :placeholder="$t('EmailPlaceholder')" />
+        </div>
+        <div class="col-2">
+            <VSelect v-model="filters.status" :items="statusItems" item-key="key" item-text="text"
+                :placeholder="$t('Filter by status')" clearable />
         </div>
     </VForm>
 </template>
--- src/views/CommentsView.vue
+++ src/views/CommentsView.vue
@@ -1,45 +1,45 @@
 <template>
-        <div class="h3 mb-4">{{ $t('Comments') }}</div>
-        <CommentsFilterForm class="mb-4" v-model="filters"/>
-        <VTableServer :items="comments" :itemsLength="comments.length" :is-loading="commentsIsLoading">
-            <VColumn :header="$t('Id')" field="id" />
-            <VColumn :header="$t('Name')" field="name" />
-            <VColumn :header="$t('Email')" field="email" />
-            <VColumn :header="$t('Post')" field="postId">
-                <template #body>
-                    <button class="btn btn-sm btn-outline-info" title="View Related Post">
-                        <i class="bi-eye" />
+    <div class="h3 mb-4">{{ $t('Comments') }}</div>
+    <CommentsFilterForm class="mb-4" v-model="filters" />
+    <VTableServer :items="comments" :itemsLength="comments.length" :is-loading="commentsIsLoading">
+        <VColumn :header="$t('Id')" field="id" />
+        <VColumn :header="$t('Name')" field="name" />
+        <VColumn :header="$t('Email')" field="email" />
+        <VColumn :header="$t('Post')" field="postId">
+            <template #body>
+                <button class="btn btn-sm btn-outline-info" title="View Related Post">
+                    <i class="bi-eye" />
+                </button>
+            </template>
+        </VColumn>
+        <VColumn :header="$t('Text')" field="body" />
+        <VColumn :header="$t('Status')" field="id">
+            <template #body="{ item }">
+                <span v-if="item.status === 'PENDING'">
+                    {{ $t('Pending') }}
+                </span>
+                <span v-if="item.status === 'CONFIRMED'">
+                    {{ $t('Confirmed') }}
+                </span>
+                <span v-if="item.status === 'REJECTED'">
+                    {{ $('Rejected') }}
+                </span>
+            </template>
+        </VColumn>
+        <VColumn :header="$t('Actions')" field="id">
+            <template #body>
+                <div class="d-flex gap-2">
+                    <button class="btn btn-danger btn-sm">
+                        <i class="bi-ban" />
                     </button>
-                </template>
-            </VColumn>
-            <VColumn :header="$t('Text')" field="body" />
-            <VColumn :header="$t('Status')" field="id">
-                <template #body="{ item }">
-                    <span v-if="item.status==='PENDING'">
-                        {{ $t('Pending') }}
-                    </span>
-                    <span v-if="item.status==='CONFIRMED'">
-                        {{$t('Confirmed')}}
-                    </span>
-                    <span v-if="item.status==='REJECTED'">
-                        {{ $('Rejected') }}
-                    </span>
-                </template>
-            </VColumn>
-            <VColumn :header="$t('Actions')" field="id">
-                <template #body>
-                    <div class="d-flex gap-2">
-                        <button class="btn btn-danger btn-sm">
-                            <i class="bi-ban" />
-                        </button>
-                        <button class="btn btn-success btn-sm z-3" data-bs-toggle="tooltip" data-bs-placement="top"
-                            data-bs-title="Tooltip on top">
-                            <i class="bi-check-circle" />
-                        </button>
-                    </div>
-                </template>
-            </VColumn>
-        </VTableServer>
+                    <button class="btn btn-success btn-sm z-3" data-bs-toggle="tooltip" data-bs-placement="top"
+                        data-bs-title="Tooltip on top">
+                        <i class="bi-check-circle" />
+                    </button>
+                </div>
+            </template>
+        </VColumn>
+    </VTableServer>
 </template>
 
 <script>
@@ -48,30 +48,38 @@ import VColumn from '@/components/data-table/VColumn.vue';
 import VTableServer from '@/components/data-table/VTableServer.vue';
 import { useFetchComments } from '@/composables/comments.composable';
 import { useApplyFilters } from '@/composables/filter.composable';
-import { watch } from 'vue';
+import { computed } from 'vue';
 
 export default {
     name: 'CommentsView',
     setup() {
         const { comments, commentsIsLoading, fetchComments } = useFetchComments()
 
-         const filters = useApplyFilters({
-                querySearch: undefined,
-                status: undefined,
-                email: undefined
+        fetchComments({})
+
+        const filters = useApplyFilters({
+            searchQuery: undefined,
+            status: undefined,
+            email: undefined
+        });
+
+        const filteredComments = computed(() => {
+            const enteredEmail = filters?.email && filters.email.trim().toLowerCase();
+            const query = filters?.searchQuery && isNaN(Number(filters.querySearch)) ? filters.searchQuery.trim().toLowerCase() : undefined;
+            const status = filters?.status && filters.status.trim();
+
+            const matchesEmail = (email) => enteredEmail && email.trim().toLowerCase().includes(enteredEmail)
+            const matchesQuery = (id,body,name)=> id=== Number(query) || body.trim().toLowerCase().includes(query) || name.trim().toLowerCase().includes(query)
+
+            return comments.value?.filter(comment => {
+                if(!query && !enteredEmail && !status) return true;
+                return matchesEmail(comment.email) || matchesQuery(comment.id, comment.body, comment.name) || status === comment.status;
             });
+        });
 
 
-        function fetch() {
-            const config = {}
-            return fetchComments(config)
-        }
-        fetch()
-        watch([], () => fetchComments())
-
-       
         return {
-            comments,
+            comments: filteredComments,
             commentsIsLoading,
             filters
         }
